6159588b92640265ae335145b827e25ea35e4d2e
aaculovich <cat@ater.me>
2019-10-10 02:10:06 +0300

    add empty template

---
+++ 0000-empty/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func solve(rdr io.Reader, wrr io.Writer) {
+	var (
+		n int
+	)
+	fmt.Fscan(rdr, &n)
+
+	fmt.Fprintln(wrr, 42)
+}
+
+func main() {
+	const MB = 1024 * 1024
+	rdr := bufio.NewReaderSize(os.Stdin, 4*MB)
+	wrr := bufio.NewWriterSize(os.Stdout, 4*MB)
+	defer wrr.Flush()
+
+	solve(rdr, wrr)
+}
---
+++ 0000-empty/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/atercattus/hackerrank"
+	"testing"
+)
+
+func Test_solve(t *testing.T) {
+	hackerrank.Checker(solve, t, `
+`, `
+`)
+}
